624465396b5d8e30624114a63decdeb8479dfbd1
JohnOkari <johnnokari@gmail.com>
2023-07-05 22:42:38 +0300

    add customer

---
+++ src/components/Customer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="customer">
+    <h2>{{ customer.name }}</h2>
+    <p>ID: {{ customer.id }}</p>
+    <p>Phone: {{ customer.phone }}</p>
+    <p>Email: {{ customer.email }}</p>
+    <p>Member Number: {{ customer.memberNumber }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    customer: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.customer {
+  margin-bottom: 20px;
+}
+</style>
